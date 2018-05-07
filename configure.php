2477d53e26a0573851d8f47fd02d51eccc431bd2
t_matsushita <matsushita@ti-am.jp>
2018-05-07 17:01:03 +0900

    remove configure

--- configure.php
+++
@@ -1,2 +0,0 @@
-<?php
-$filter = "LG-Web-*";
--- launch_instance.php
+++ launch_instance.php
@@ -1,13 +1,11 @@
 <?php
-require_once("./configure.php");
-
 if (empty($argv[0])) {
     echo "引数を指定してください example : php launch_instance.php start or php launch_instance.php stop";
     exit;
 }
 $state = $argv[0];
 
-$return_var = shell_exec('aws ec2 describe-instances --filter "Name=tag:Name,Values={$filter}"');
+$return_var = shell_exec('aws ec2 describe-instances --filter "Name=tag:Name,Values=LG-Web-*"');
 $instances = json_decode($return_var, true);
 
 $sttopped_instances = array();
