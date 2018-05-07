6f7e3f2ae6c37cb9c4348cef7aa1a5e1133dca56
t_matsushita <matsushita@ti-am.jp>
2018-05-07 16:51:09 +0900

    Add script

---
+++ launch_instance.php
@@ -0,0 +1,25 @@
+<?php
+require_once("./configure.php");
+
+if (empty($argv[0])) {
+    echo "引数を指定してください example : php launch_instance.php start or php launch_instance.php stop";
+    exit;
+}
+$state = $argv[0];
+
+$return_var = shell_exec('aws ec2 describe-instances --filter "Name=tag:Name,Values={$filter}"');
+$instances = json_decode($return_var, true);
+
+$sttopped_instances = array();
+foreach ($instances["Reservations"] as $instance) {
+    foreach ($instance["Instances"] as $i) {
+        if ($i["State"]["Name"] == "stopped") {
+            $sttopped_instances[] = $i["InstanceId"];
+        }
+    }
+}
+
+foreach ($sttopped_instances as $instance_id) {
+    $cmd = "aws ec2 " . $state. "-instances --instance-ids " . $instance_id;
+    shell_exec($cmd);
+}
---
+++ modify_instance_type.php
@@ -0,0 +1,23 @@
+<?php
+require_once("./configure.php");
+
+if (empty($argv[0])) {
+    echo "引数にインスタンスタイプを設定してください example : php modify_instance_type.php c4.2xlarge";
+    exit;
+}
+$instance_type = $argv[0];
+
+$return_var = shell_exec('aws ec2 describe-instances --filter "Name=tag:Name,Values={$filter}"');
+$instances = json_decode($return_var, true);
+
+$sttopped_instances = array();
+foreach ($instances["Reservations"] as $instance) {
+    foreach ($instance["Instances"] as $i) {
+        $sttopped_instances[] = $i["InstanceId"];
+    }
+}
+
+foreach ($sttopped_instances as $instance_id) {
+    $boot_cmd = "aws ec2 modify-instance-attribute --instance-id " . $instance_id . " --attribute instanceType --value " . $instance_type;
+    shell_exec($boot_cmd);
+}
