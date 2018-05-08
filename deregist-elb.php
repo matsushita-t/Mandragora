908b020b2f19a5d8614b2d1d2003e2b57ce0604e
t_matsushita <matsushita@ti-am.jp>
2018-05-08 19:57:11 +0900

    Mod

---
+++ deregist-elb.php
@@ -0,0 +1,21 @@
+<?php
+$ignore = "i-0f63c96cd2498fead"; // LG-Web-01a
+$arn = "arn:aws:elasticloadbalancing:ap-northeast-1:662684164378:targetgroup/pro-group/4b7dbda4ccc491e8";
+
+$return_var = shell_exec('aws ec2 describe-instances --filter "Name=tag:Name,Values=LG-Web-*"');
+$instances = json_decode($return_var, true);
+
+$sttopped_instances = array();
+foreach ($instances["Reservations"] as $instance) {
+    foreach ($instance["Instances"] as $i) {
+        if ($i["State"]["Name"] == "stopped") {
+            if ($i["InstanceId"] != $ignore) {
+                $sttopped_instances[] = $i["InstanceId"];
+            }
+        }
+    }
+}
+
+foreach ($sttopped_instances as $instance_id) {
+    shell_exec("aws elbv2 deregister-targets --target-group-arn " . $arn . " --targets Id=" . $instance_id);
+}
--- modify_instance_type.php
+++ modify_instance_type.php
@@ -1,17 +1,19 @@
 <?php
+$ignore = "i-0f63c96cd2498fead"; // LG-Web-01a
 if (empty($argv[0])) {
     echo "引数にインスタンスタイプを設定してください example : php modify_instance_type.php c4.2xlarge";
     exit;
 }
 $instance_type = $argv[0];
-
 $return_var = shell_exec('aws ec2 describe-instances --filter "Name=tag:Name,Values=LG-Web-*"');
 $instances = json_decode($return_var, true);
 
 $sttopped_instances = array();
 foreach ($instances["Reservations"] as $instance) {
     foreach ($instance["Instances"] as $i) {
-        $sttopped_instances[] = $i["InstanceId"];
+        if ($i["InstanceId"] != $ignore) {
+            $sttopped_instances[] = $i["InstanceId"];
+        }
     }
 }
 
